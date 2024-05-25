d6183e93b8f10618793184be30dce444c707623b
EduGese <122921699+EduGese@users.noreply.github.com>
2024-05-25 19:32:52 +0200

    Main and product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonButton,
+  IonItem,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [
+    IonApp,
+    IonRouterOutlet,
+    IonMenu,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonButton,
+    IonItem,
+    RouterLink,
+  ],
 })
 export class AppComponent {
   constructor() {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    loadComponent: () => import('./main/main.page').then((m) => m.MainPage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'product-list',
+    loadComponent: () =>
+      import('./product-list/product-list.page').then((m) => m.ProductListPage),
   },
 ];
